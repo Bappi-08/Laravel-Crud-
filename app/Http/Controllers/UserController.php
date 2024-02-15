05667a61aaa20d92394b6ed7c123947ea57b1864
SadiaAfrin1710014 <asifbappy.bd@gmail.com>
2024-02-15 16:36:24 +0600

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       $users=User::all();
+       return view('/layouts.pages.users',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    
+       
+{   return view ('layouts.pages.user_create');
+
+}
+    
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:50',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+            'phone_number' =>'required|numeric',
+        ]);
+    
+        if ($validator->fails()) {
+            return redirect('/user/create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+    
+       
+    
+       // $filepath = $request->image->storeAs('uploads',  $request->image->getClientOriginalName());
+    
+    
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone_number = $request->phone_number;
+        $user->password = bcrypt($request->password);
+      //  $user->image = $filepath;  // Corrected line
+        $user->save();
+    
+        return redirect()->to('/users')->with('a', 'User created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $user=User::findOrFail($id);
+        return view('layouts.pages.user_edit',compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:50',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+            'phone_number' =>'required|numeric',
+        ]);
+    
+        if ($validator->fails()) {
+            return redirect('/user/$id/edit')
+                ->withErrors($validator)
+                ->withInput();
+        }
+    
+       
+        $user=User::find($id);
+        $user->name=$request->name;
+        $user->name=$request->email;
+        $user->password=bcrypt($request->password);
+        $user->save();
+        
+        return redirect()->to('/users')->with('a', 'User updated  successfully');
+    
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user=User::find($id);
+        $user->delete();
+        return redirect()->to('/users')->with('a', 'Userd deleted  successfully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -34,90 +36,20 @@ Route::get('/layouts.masters',function()
 {
 
 });
-Route::get('/users',function()
-
-{   $users=User::all();
-    
-    
-    
-    return view('/layouts.pages.users',compact('users'));
-});
+Route::get('/users',[UserController::class,'index']);
 Route::get('/user/{id}/show',function($id)
 {   
     $user=User::findOrFail($id);
     return view('layouts.pages.user_details',compact('user'));
 
 });
-Route::get('user/create',function()
-{   return view ('layouts.pages.user_create');
+Route::get('user/create',[UserController::class,'create']);
 
-});
-
-Route::post('user/store', function (Request $request) {  
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:50',
-        'email' => 'required|email',
-        'password' => 'required|confirmed',
-        'phone_number' =>'required|numeric',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/user/create')
-            ->withErrors($validator)
-            ->withInput();
-    }
-
+Route::post('user/store', [UserController::class,'store']);
+Route::get('/user/{id}/edit',[UserController::class,'edit']);
+ 
    
 
-   // $filepath = $request->image->storeAs('uploads',  $request->image->getClientOriginalName());
 
-
-    $user = new User();
-    $user->name = $request->name;
-    $user->email = $request->email;
-    $user->phone_number = $request->phone_number;
-    $user->password = bcrypt($request->password);
-  //  $user->image = $filepath;  // Corrected line
-    $user->save();
-
-    return redirect()->to('/users')->with('a', 'User created successfully');
-});
-Route::get('/user/{id}/edit',function($id)
-{   
-    $user=User::findOrFail($id);
-    return view('layouts.pages.user_edit',compact('user'));
-
-});
-Route::post('user/update/{id}', function (Request $request,$id){
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:50',
-        'email' => 'required|email',
-        'password' => 'required|confirmed',
-        'phone_number' =>'required|numeric',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/user/$id/edit')
-            ->withErrors($validator)
-            ->withInput();
-    }
-
-   
-    $user=User::find($id);
-    $user->name=$request->name;
-    $user->name=$request->email;
-    $user->password=bcrypt($request->password);
-    $user->save();
-    
-    return redirect()->to('/users')->with('a', 'User updated  successfully');
-
-
-
-});
-Route::get('/user/{id}/delete',function($id)
-{   
-   $user=User::find($id);
-    $user->delete();
-    return redirect()->to('/users')->with('a', 'Userd deleted  successfully');
-
-});
+Route::post('user/update/{id}', [UserController::class,'update']);
+Route::get('/user/{id}/delete',[UserController::class,'destroy']);
